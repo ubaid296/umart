92c16fa25b599cc3de112aaf1b90bf282ab6b619
ubaid296 <chubaid170@gmail.com>
2020-05-06 18:21:42 +0100

    dynamically data pass in item

--- src/components/common/itemCard.jsx
+++ src/components/common/itemCard.jsx
@@ -2,37 +2,46 @@ import React, { Component } from "react";
 
 class ItemCard extends Component {
   render() {
+    const {
+      title,
+      productWeight,
+      productPrice,
+      productSale,
+      path,
+    } = this.props.item;
+
     return (
       <div className="row item-card">
         <div className="w100">
           <img
-            src="/images/bananas.jpg"
+            src={path}
             className="align-center item-img"
             alt="item"
+            height="200px"
           />
         </div>
         <div className="w100">
           <div className="w100">
-            <p>Banana</p>
-            <p>8(pc)s</p>
+            <p className="item-title">{title}</p>
+            <p className="item-quantity">{productWeight}</p>
           </div>
           <div className="w100">
-            <strike>$8</strike>
+            <strike className="item-sale">$8</strike>
           </div>
           <div className="w100">
             <div className="row">
               <div className="col-6">
-                <p>$10</p>
+                <p className="item-price">{productPrice}</p>
               </div>
               <div className="col-6">
                 <div className="row item-cart">
                   <img
                     src="/images/icons/cart2.png"
                     alt="cart"
-                    height="20px"
+                    height="22px"
                     className="cart-img"
                   />
-                  <p>Cart</p>
+                  <p className="item-margencart">Cart</p>
                 </div>
               </div>
             </div>
--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -5,47 +5,78 @@ class ItemList extends Component {
   state = {
     item: [
       {
-        id: 1,
         title: "banana",
         productWeight: "8pc(s)",
         productPrice: "$10",
+        productSale: "$3",
         path: "/images/bananas.jpg",
         titleClass: "item-title",
+        quantityClass: "item-qunatity",
+        priceClass: "item-price",
+        saleClass: "item-sale",
       },
       {
-        id: 2,
         title: "Onion",
         productWeight: "8pc(s)",
         productPrice: "$6",
+        productSale: "$5",
         path: "/images/onion.jpg",
+        quantityClass: "item-qunatity",
+        priceClass: "item-price",
       },
       {
-        id: 3,
         title: "Grapes",
         productWeight: "5pc(s)",
         productPrice: "$15",
+        productSale: "$10",
         path: "/images/grapes.jpg",
+        quantityClass: "item-qunatity",
+        priceClass: "item-price",
       },
       {
-        id: 4,
         title: "Coconut",
         productWeight: "10pc(s)",
         productPrice: "$7",
+        productSale: "$2",
         path: "/images/coconut.jpg",
+        quantityClass: "item-qunatity",
+        priceClass: "item-price",
       },
       {
-        id: 5,
         title: "Garlis",
         productWeight: "8pc(s)",
         productPrice: "$20",
+        productSale: "$10",
         path: "/images/garlic.jpg",
+        quantityClass: "item-qunatity",
+        priceClass: "item-price",
       },
       {
-        id: 6,
         title: "GreenApple",
         productWeight: "12pc(s)",
         productPrice: "$10",
+        productSale: "$4",
         path: "/images/apple.jpg",
+        quantityClass: "item-quantity",
+        priceClass: "item-price",
+      },
+      {
+        title: "Lemon",
+        productWeight: "12pc(s)",
+        productPrice: "$10",
+        productSale: "$4",
+        path: "/images/lemon.jpg",
+        quantityClass: "item-quantity",
+        priceClass: "item-price",
+      },
+      {
+        title: "Oranges",
+        productWeight: "12pc(s)",
+        productPrice: "$10",
+        productSale: "$4",
+        path: "/images/orange.jpg",
+        quantityClass: "item-quantity",
+        priceClass: "item-price",
       },
     ],
   };
@@ -53,7 +84,7 @@ class ItemList extends Component {
     return (
       <div className="row p-5">
         {this.state.item.map((data) => (
-          <div key={data.id} className="col-3">
+          <div className="col-3">
             <ItemCard item={data} />
           </div>
         ))}
